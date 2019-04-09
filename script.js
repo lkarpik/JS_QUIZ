8d7d3d1fdbbe657137f39f65cfe4e2d552633fd3
Lukasz Karpik <lkarpik@wp.pl>
2019-04-09 20:44:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * Quiz Controller
+ */
+let quizController = (function(){
+    
+    /**
+     * Question constructor
+     * 
+     */
+    function Question(id, questionText, options, correctAnswer) {
+        
+    }
+})();
+
+/**
+ * UI Controller
+ */
+let UIController = (function(){
+    
+})();
+
+/**
+ * Controller
+ */
+let controller = (function(quizCtrl, UICtrl){
+    
+
+})(quizController, UIController);
