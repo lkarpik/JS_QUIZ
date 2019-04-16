6e9891f4b69c944361119df5dee7f644132617a0
Lukasz Karpik <lkarpik@wp.pl>
2019-04-16 13:05:13 +0200

    Starting tempalte

--- script.js
+++ script.js
@@ -1,43 +1,10 @@
-/**
- * Quiz Controller
- */
-let quizController = (function(){
-    
-    /**
-     * Question constructor
-     */
-    
-    function Question(id, questionText, options, correctAnswer) {
-        this.id = id;
-        this.questionText = questionText;
-        this.options = options;
-        this.correctAnswer = correctAnswer;
-    }
-    
-})();
+(function(name){
+    console.log("Hello "+ name );
+})('John');
 
-/**
- * UI Controller
- */
-let UIController = (function(){
-    
-    let domItems = {
-        // Admin Panel Elements
-        questInsertBtn: document.getElementById("question-insert-btn")
-    };
-    return {
-        getDomItems: domItems
-    };
-
-})();
-
-/**
- * Controller
- */
-let controller = (function(quizCtrl, UICtrl){
-    UICtrl.getDomItems.questInsertBtn.addEventListener("click", function () {
-        console.log(this);
-    });
-    
-
-})(quizController, UIController);
+let a = function() {
+    let b = "Hello b";
+    let c = "Hello c";
+    console.log("Hello from a");
+    return arr=b ;
+}();
